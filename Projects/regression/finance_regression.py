4e07ab917ae9b35e31d06059c492ff22d3b1e9a9
Akshay184 <akshayagarwal184@gmail.com>
2019-06-26 14:37:55 +0530

    Linear Regression

--- Projects/regression/finance_regression.py
+++ Projects/regression/finance_regression.py
@@ -17,19 +17,22 @@ import sys
 import pickle
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
-dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
+dictionary = pickle.load( open("../tools/final_project_dataset_modified_unix.pkl", "rb") )
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
+
 features_list = ["bonus", "salary"]
-data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
+# features_list = ["bonus","long_term_incentive"]
+
+data = featureFormat( dictionary, features_list, remove_any_zeroes=True, sort_keys='../tools/python2_lesson06_keys_unix.pkl')
 target, features = targetFeatureSplit( data )
 
 ### training-testing split needed in regression, just like classification
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -38,9 +41,19 @@ test_color = "b"
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
 
+# importing the linear regression from sklearn
+from sklearn.linear_model import LinearRegression
 
+# creating a regression model
+reg = LinearRegression()
 
+# fiting the data
+reg.fit(feature_train, target_train)
 
+print("Regression coefficient is:",reg.coef_)
+print("Intercept is:",reg.intercept_)
+print("Score on the training data is:",reg.score(feature_train, target_train))
+print("Score on the test data is:",reg.score(feature_test, target_test))
 
 
 
@@ -64,7 +77,15 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b")
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
 plt.show()
+
+
+print("Slope of new regression line:",reg.coef_)
+print("Intercept of new regression line:",reg.intercept_)
+print("Score of this regression:",reg.score(feature_test, target_test))
+
